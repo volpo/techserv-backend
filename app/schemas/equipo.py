830a643a0d2573beaf9dad1efae63034b50d7ba5
Marcelo <marcelonjurado@gmail.com>
2026-06-09 20:07:57 -0300

    Se creo endpoint /equipos

--- app/schemas/equipo.py
+++ app/schemas/equipo.py
@@ -8,4 +8,15 @@ class Equipo(BaseModel):
     marca: str = Field(min_length=1, max_length=255)
     modelo: str = Field(min_length=1, max_length=255)
     numero_serie: str = Field(min_length=1, max_length=255)
-    cliente_id: uuid.UUID
+    fecha_creacion: datetime | None = None
+    cliente_id: uuid.UUID | None = None
+
+class EquipoRead(Equipo):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: uuid.UUID
+
+class EquipoCreate(Equipo):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: uuid.UUID | None = None
